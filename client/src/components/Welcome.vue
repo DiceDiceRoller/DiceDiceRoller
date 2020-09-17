26570f8f224333ae5d3f35b5a678fe4785440035
mohammad rendra <abuabdillatief@gmail.com>
2020-09-18 00:40:27 +0700

    input name

--- client/src/components/Welcome.vue
+++ client/src/components/Welcome.vue
@@ -7,18 +7,32 @@
   <lottie-player class="mx-auto" src="https://assets7.lottiefiles.com/packages/lf20_5GDwcV.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
   </div>
 </div>
-
 </template>
 
 <script>
+import swal from 'sweetalert'
 export default {
   name: 'Welcome',
+  data: () => {
+    return {
+      playerName: ''
+    }
+  },
   props: {
     msg: String
   },
   methods: {
-    moveInside(){
-      this.$router.push('/home')
+    moveInside () {
+      swal('What should we call you?', {
+        content: 'input'
+      })
+        .then((value) => {
+          this.playerName = value
+          swal(`Alright ${value}, let's get it!`)
+            .then(value => {
+              this.$router.push('/home')
+            })
+        })
     }
   }
 }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-<div class="container"></div>
+<div class="container">
+</div>
 </template>
 
 <script>
+// import Swal from 'sweetalert2'
 import swal from 'sweetalert'
 export default {
   name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
   created () {
     swal('RULES:\n\n- Each player has right to play as many turns as he/she likes \n\n- Points will be firstly recorded as "current points"\n\n- When player decides to end his/her turn, he/she will now hold that current points into "accumulated points"\n\n- Whenever number 1 is drawn, current point will be cleared and the other player will have his/her turn\n\n- IF number 6 are drawn in both dices, current and accumulated ponits will be set back to 0.\n\n\n HAPPY PLAYING!')
   }
