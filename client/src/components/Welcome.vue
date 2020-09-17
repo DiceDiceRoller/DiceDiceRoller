0d0bb0f49d17a0d7fa37270b9c642d48f2441ddd
mohammad rendra <abuabdillatief@gmail.com>
2020-09-17 20:22:47 +0700

    Welcome Page

---
+++ client/src/components/Welcome.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="welcome-container mx-auto">
+  <div class="animation row">
+  <lottie-player class="mx-auto" src="https://assets7.lottiefiles.com/packages/lf20_5GDwcV.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
