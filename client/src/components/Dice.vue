8cc58bb1d74c29eca08c4c6ad5e9aeeaeaca7dfc
ArdySitio <ardy.ai29@gmail.com>
2020-09-18 10:43:55 +0700

    update component dice

--- client/src/components/Dice.vue
+++ client/src/components/Dice.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="imgss">
             <h3 v-if="start" @click="startGame">Start game</h3>
-            <img :src="imgs" :style="st" @click="getRandom">
+            <img :src="image" :style="st" @click="getRandom">
         </div>
 
         <div class="content" v-if="!start">
@@ -17,21 +17,33 @@
 </template>
 
 <script>
+import DiceOne from '../assets/dice-1.png'
+import DiceTwo from '../assets/dice-2.png'
+import DiceThree from '../assets/dice-3.png'
+import DaduFour from '../assets/dice-4.png'
+import DaduFive from '../assets/dice-5.png'
+import DaduSix from '../assets/dice-6.png'
 export default {
   name: 'app',
   data () {
     return {
       start: true,
-      imgs: '',
+      image: '',
       firstUser: 0,
       secondUser: 0,
-      st: ''
+      st: '',
+      dice1: DiceOne,
+      dice2: DiceTwo,
+      dice3: DiceThree,
+      dice4: DaduFour,
+      dice5: DaduFive,
+      dice6: DaduSix
     }
   },
   methods: {
     getRandom () {
       if (this.secondUser >= 50) {
-        alert('Sytem won game' + ' ' + this.secondUser)
+        alert('Another player won game' + ' ' + this.secondUser)
         this.secondUser = 0
         this.firstUser = 0
         this.resetGame()
@@ -47,51 +59,51 @@ export default {
       const num = Math.floor(Math.random() * 6) + 1
       switch (num) {
         case 1:
-          this.imgs = '../assets/dice-1.png'
+          this.image = DiceOne
           this.st = {
             border: '4px doubled #1abc9c'
           }
           this.firstUser += num
           break
         case 2:
-          this.imgs = '../assets/dice-2.png'
+          this.image = DiceTwo
           this.firstUser += num
           this.st = {
-            border: '4px dotted #2ecc71'
+            border: '4px #2ecc71'
           }
           break
         case 3:
-          this.imgs = '../assets/dice-3.png'
+          this.image = DiceThree
           this.firstUser += num
           this.st = {
-            border: '4px dotted #3498db'
+            border: '4px #3498db'
           }
           break
         case 4:
-          this.imgs = '../assets/dice-4.png'
+          this.image = DaduFour
           this.firstUser += num
           this.st = {
-            border: '4px dotted rgba(239, 87, 119,1.0)'
+            border: '4px rgba(239, 87, 119,1.0)'
           }
           break
         case 5:
-          this.imgs = '../assets/dice-5.png'
+          this.image = DaduFive
           this.firstUser += num
           this.st = {
-            border: '4px dotted #e74c3c'
+            border: '4px #e74c3c'
           }
           break
         case 6:
-          this.imgs = '../assets/dice-6.png'
+          this.image = DaduSix
           this.firstUser += num
           this.st = {
-            border: '4px dotted #ecf0f1'
+            border: '4px #ecf0f1'
           }
           break
       }
-      this.SytemRandom()
+      this.diceSecondUser()
     },
-    SytemRandom () {
+    diceSecondUser () {
       const num = Math.floor(Math.random() * 6) + 1
       switch (num) {
         case 1:
@@ -127,15 +139,15 @@ export default {
     },
     resetGame () {
       this.secondUser = 0
-      this.user = 0
-      this.imgs = null
+      this.firstUser = 0
+      this.image = null
       this.st = null
       this.start = true
     },
     startGame () {
       this.start = false
       this.secondUser = 0
-      this.user = 0
+      this.firstUser = 0
       this.getRandom()
     }
   },
@@ -166,26 +178,8 @@ button:hover {
   box-shadow: inset 20px 2px 20px 0px transparent;
   background-color: #a51717;
 }
-.default h1{
-
-   color: rgb(221, 202, 32);
-   text-align: left;
-}
-.second {
-  color: white;
- background-image: linear-gradient(black,#907f4f);
-  padding: 2rem;
-  position: relative;
-  font-family: cursive;
-  height: 100vh;
-  text-transform: capitalize;
-}
-.second h1{
-  text-align: right;
-   color:yellow;
-}
 .imgss {
-  box-shadow: -20px 10px 2px 0px rgb(219, 221, 223);
+  box-shadow: 5px 3px 2px 0px rgb(219, 221, 223);
   border: 2.9px double rgba(243, 241, 241, 0.712);
   border-radius: 20px;
   padding: 1rem 1rem 1rem 1rem;
@@ -195,20 +189,15 @@ button:hover {
   height: 170px;
   margin: 0 auto;
   transition: all 0.3s;
-  z-index: 100;
   cursor: pointer;
 }
 img {
   border-radius: 20px;
   box-shadow: 2px 10px 10px black;
   transition: all 0.3s;
-}
-.imgss:hover {
-  box-shadow: none;
-}
-.imgss:hover > img {
-  box-shadow: 0px 7px 10px #fff;
-  padding: 2px;
+  width: 10rem;
+  margin-top: 11rem;
+  margin-right: 2rem;
 }
 .score {
   background-color: #dfff;
@@ -219,7 +208,7 @@ img {
   margin:2rem auto;
 }
 .content {
-  margin-top: 5rem;
+  margin-top: 13rem;
 }
 h3 {
   cursor: pointer;
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,11 +13,6 @@ export default {
   components: {
     Dices
   },
-  data () {
-    return {
-      // dice: [5, 6]
-    }
-  },
   created () {
     swal('RULES:\n\n- Each player has right to play as many turns as he/she likes \n\n- Points will be firstly recorded as "current points"\n\n- When player decides to end his/her turn, he/she will now hold that current points into "accumulated points"\n\n- Whenever number 1 is drawn, current point will be cleared and the other player will have his/her turn\n\n- IF number 6 are drawn in both dices, current and accumulated ponits will be set back to 0.\n\n\n HAPPY PLAYING!')
   }
