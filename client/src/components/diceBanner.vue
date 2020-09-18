77d7983a7babd7cb17091dc0f2d6486b86b43cbc
mohammad rendra <abuabdillatief@gmail.com>
2020-09-18 10:40:47 +0700

    ganti styles.css

--- client/src/components/diceBanner.vue
+++ client/src/components/diceBanner.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <lottie-player class="mx-auto ml-2" src="https://assets10.lottiefiles.com/private_files/lf30_YQxjey.json"  background="transparent"  speed="1"  style="width: 500px; height: 300px;"  loop  autoplay></lottie-player>
-       <lottie-player class="mx-auto ml-2" src="https://assets10.lottiefiles.com/private_files/lf30_YQxjey.json"  background="transparent"  speed="1"  style="width: 500px; height: 300px;"  loop  autoplay></lottie-player>
+    <div class="lottie-banner">
+       <lottie-player class="mx-auto player-1" src="https://assets10.lottiefiles.com/private_files/lf30_YQxjey.json"  background="transparent"  speed="1"  style="width: 500px; height: 300px;"   autoplay></lottie-player>
+       <lottie-player class="mx-auto" src="https://assets10.lottiefiles.com/private_files/lf30_YQxjey.json"  background="transparent"  speed="1"  style="width: 500px; height: 300px;"   autoplay></lottie-player>
     </div>
   </div>
 </template>
